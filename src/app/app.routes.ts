cacdc5b6abfe60b618940f3bba35117a6b9d910f
Ferrerov <valenferrero01@gmail.com>
2024-09-05 09:55:18 -0300

    Agregado de bootstrap, angular material y componente registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { QuiensoyComponent } from './components/quiensoy/quiensoy.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'login', component: LoginComponent},
     { path: 'home', component: HomeComponent},
+    { path: 'registro', component: RegistroComponent},
     { path: 'quiensoy', component: QuiensoyComponent},
+    
 ];
