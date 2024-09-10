68c80963b81b7ec5426f169acfbe77966c8ec829
Ferrerov <valenferrero01@gmail.com>
2024-09-10 19:41:41 -0300

    Conexion con api de Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"saladejuegosferrero-12fef","appId":"1:79579890824:web:90dce5f449b0139aa8cb3d","storageBucket":"saladejuegosferrero-12fef.appspot.com","apiKey":"AIzaSyCmwQ_l6hO17XPE7kst2B343CClIvP4mx0","authDomain":"saladejuegosferrero-12fef.firebaseapp.com","messagingSenderId":"79579890824"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
